7ad706bf71b1fc40452ffdca2cbabd11c66a3ca2
madortill <mador.till@gmail.com>
2025-11-19 11:39:04 +0200

    now work

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './style/App.jsx'
+import './style/index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
